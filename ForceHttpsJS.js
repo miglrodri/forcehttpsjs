8c0bd663d8d77d757f46f5ccf38eeb0f7717dea1
Miguel Jesus <migl.rodri@gmail.com>
2018-07-10 00:11:53 +0100

    future add jquery dependency

--- ForceHttpsJS.js
+++ ForceHttpsJS.js
@@ -1,6 +1,11 @@
 // Force Http JS
 // V: 0.1
 
+// window.onload appears to be the most widely supported. In fact, some of the most modern browsers have in a sense replaced document.onload with window.onload. Browser support issues are most likely the reason why many people are starting to use libraries such as jQuery to handle the checking for the document being ready like so:
+
+// $(document).ready(function() { /* code here */ });
+// $(function() { /* code here */ });
+
 
 //console.log("current protocol: ", location.protocol);
 if (location.protocol != 'https:')
